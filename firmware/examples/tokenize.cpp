1d775f08de498f81277f357b65a03f7ec1b74c09
Josh Hollenbeck <josh.hollenbeck@citrusbyte.com>
2014-08-13 00:20:32 -0700

    simple example borrowed from amamchur

---
+++ firmware/examples/tokenize.cpp
@@ -0,0 +1,19 @@
+#include "jsonlite.h"
+
+void setup() {
+  const int JSON_DEPTH = 4;                                                   // Limitation of JSON depth
+  char json[] = "[-1, 0, 1, true, false, null]";                              // JSON to validate
+  size_t mem_used = jsonlite_parser_estimate_size(JSON_DEPTH);                // Estimate memory usage
+  printf("jsonlite will use %zd bytes of RAM for JSON validation", mem_used);
+  jsonlite_parser p = jsonlite_parser_init(JSON_DEPTH);                       // Init parser with specified depth
+  jsonlite_result result = jsonlite_parser_tokenize(p, json, sizeof(json));   // Check JSON
+  // Check result
+  if (result == jsonlite_result_ok) {
+    // do stuff
+  }
+  jsonlite_parser_release(p);
+}
+
+void loop() {
+
+}
